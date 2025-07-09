d6bd9dfc58e3a20e76c30a32dba813d00efac018
Dickyyy2 <dickyroby@students.amikom.ac.id>
2025-07-09 14:13:15 +0700

    fix vite

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,5 @@ app.get("/api/education", (req, res) => res.json(educationHistory));
 app.get("/api/skills", (req, res) => res.json(skills));
 app.get("/api/projects", (req, res) => res.json(projects));
 app.listen(PORT, () => {
-  console.log(` Server backend berjalan di
-http://localhost:${PORT}`);
+  console.log(` Server backend berjalan di http://localhost:${PORT}`);
 });
